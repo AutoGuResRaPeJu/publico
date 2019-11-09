1a0b34415539bfe14a908c268e7db68ecd501a1c
JULIO CESAR ESCODRO FILHO <28424@fiecalunos.lan>
2019-11-08 22:51:55 -0300

    mudando o DAO

--- src/models/casaDAO.js
+++ src/models/casaDAO.js
@@ -2,11 +2,8 @@ const con = require('../helpers/banco');
 
 class CasaDAO {
    buscaPorComponenteECasa(casa, cb){
-       const sql = "select * from tb_usuarios";
-       con.query(sql, [], (err,res) => {
-            if(err) throw err;
-            else cb(res);
-       });
+     
+       
    }
 }
 
--- src/models/usuarioDAO.js
+++ src/models/usuarioDAO.js
@@ -2,11 +2,7 @@ const con = require('../helpers/banco');
 
 class UsuarioDAO {
    buscaPorComponenteECasa(usuario, cb){
-       const sql = "select * from tb_usuarios";
-       con.query(sql, [], (err,res) => {
-            if(err) throw err;
-            else cb(res);
-       });
+       
    }
 }
 
